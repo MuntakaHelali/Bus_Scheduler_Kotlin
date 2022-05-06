c7de00fdbde8e785cd80aa8f2b75bdbc373c01f1
Muntaka Helali <helali.muntaka@gmail.com>
2022-05-06 07:50:33 -0400

    Defined the viewModel class so that it can store data related to the UI, and also be lifecycle aware.

---
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.busschedule.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.busschedule.database.Schedule
+import com.example.busschedule.database.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel(){
+    fun fullSchedule():List<Schedule> = scheduleDao.getAll()
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+}
+
+/*
+Can instantiate a BusScheduleViewModelFactory object with BusScheduleViewModelFactory.create()
+soo that the viewModel can be lifecycle aware without the fragment having to handle it directly
+*/
+
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
