9f015ec1bbc9352c376b7ce79bd0fa245adcb3c7
Muntaka Helali <helali.muntaka@gmail.com>
2022-05-06 08:59:51 -0400

    Implemented database to respond to data changes using Flow with the return type for the DAO and the ScheduleViewModel where the DAO functions are called.

--- app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
+++ app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
@@ -21,6 +21,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.databinding.StopScheduleFragmentBinding
@@ -29,6 +30,7 @@ import com.example.busschedule.viewmodels.BusScheduleViewModelFactory
 import com.example.busschedule.viewmodels.BusStopAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class StopScheduleFragment: Fragment() {
@@ -79,8 +81,13 @@ class StopScheduleFragment: Fragment() {
 // call from potentially locking the UI, you should use a
 // coroutine scope to launch the function. Using GlobalScope is not
 // best practice, and in the next step we'll see how to improve this.
-        GlobalScope.launch (Dispatchers.IO) {
-            busStopAdapter.submitList(viewModel.scheduleForStopName(stopName))
+//        GlobalScope.launch (Dispatchers.IO) {
+//            busStopAdapter.submitList(viewModel.scheduleForStopName(stopName))
+//        }
+        lifecycle.coroutineScope.launch {
+            viewModel.scheduleForStopName(stopName).collect() {
+                busStopAdapter.submitList(it)
+            }
         }
 
     }
